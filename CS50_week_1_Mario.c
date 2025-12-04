08015e6f97da47aa5a4136b1cb39c7fe517cc16e
danielbeale-ITguy <danielbeale719@gmail.com>
2025-12-04 21:33:35 +1100

    Update CS50_week_1_Mario.c

--- CS50_week_1_Mario.c
+++ CS50_week_1_Mario.c
@@ -18,5 +18,27 @@ int main(){
         }
         
 
+// below code is test from another file that can be used to implement the backwards function.
 
+#include <stdio.h>
+#include <string.h>
 
+int main(){
+        char word[10] = "Daniel";
+        int lenght = strlen(word);
+        int base = 0;
+        int end = lenght ;
+
+        //printf("%s\n",word);
+        //printf("%c\n",word[0]);
+
+        while (end >= base){
+        //assign to variable then printf possibly       
+                printf("%c",word[end]);
+                end --;
+        
+        }
+
+        printf("\n");//prints the actual line 
+        return 0;
+}

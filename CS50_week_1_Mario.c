1f4da73779a32fa3c0bb594ce223c4ec211abe6b
danielbeale-ITguy <danielbeale719@gmail.com>
2025-12-07 19:34:19 +1100

    Update CS50_week_1_Mario.c
    Hash's sort of done, need to remove extra and make the x's into spaces

--- CS50_week_1_Mario.c
+++ CS50_week_1_Mario.c
@@ -2,23 +2,34 @@
 
 int main(){
 
-        char dot_line[] = "X";
-        int height = 8;
-        int len;
-        int base = 0;
+	char dot_line[] = "X";
+	int hash; 
+	int height = 8;
+	int len;
+	int base = 0;
+	int count = 0;
 
-        //use double for loop to fill space with x, later making it _
-        while (height >= 0){
-                len = height;
+	//use double for loop to fill space with x, later making it _
+	while (height >= 0){
+		len = height;
+		
+		while (len >=  base) {
+				printf("x");
+				
+				len --;	
+			}
+	
+	//creating the hashes	
+		count ++;		
+		for (hash = 0; hash <= count; hash ++)	{
+				printf("#");
+			}
 
-                while (len >=  base) {
-                                printf("x");
-                                len --;
-                        }
-
-                        printf("\n");
-                        height --;
-        }
-
+			
+			
+			printf("\n");
+			height --;
+			
+	}
 
 }

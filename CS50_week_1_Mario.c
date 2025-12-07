008ff32637bf2b676b40c2f1ee052f42e1f3ea68
danielbeale-ITguy <danielbeale719@gmail.com>
2025-12-07 18:14:10 +1100

    Update CS50_week_1_Mario.c

--- CS50_week_1_Mario.c
+++ CS50_week_1_Mario.c
@@ -1,44 +1,24 @@
 #include <stdio.h>
-#include <string.h>
-
-
 
 int main(){
 
-        int num = 3;
-        char sign[30] = "#";
-        char new_sign[30];
-
-
-        while (num > 0){
-                
-                strcat(new_sign,sign);
-                printf("%s\n",new_sign);
-                num -= 1;
-        }
-        
-
-// below code is test from another file that can be used to implement the backwards function.
-
-#include <stdio.h>
-#include <string.h>
-
-int main(){
-        char word[10] = "Daniel";
-        int lenght = strlen(word);
+        char dot_line[] = "X";
+        int height = 8;
+        int len;
         int base = 0;
-        int end = lenght ;
 
-        //printf("%s\n",word);
-        //printf("%c\n",word[0]);
+        //use double for loop to fill space with x, later making it _
+        while (height >= 0){
+                len = height;
 
-        while (end >= base){
-        //assign to variable then printf possibly       
-                printf("%c",word[end]);
-                end --;
-        
+                while (len >=  base) {
+                                printf("x");
+                                len --;
+                        }
+
+                        printf("\n");
+                        height --;
         }
 
-        printf("\n");//prints the actual line 
-        return 0;
+
 }

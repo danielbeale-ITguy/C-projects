91dfb3f5e9e8ad4ba9dc55268a2aa0bf7d7c1f0d
danielbeale-ITguy <danielbeale719@gmail.com>
2025-12-08 22:23:31 +1100

    Create directory_reader.c

---
+++ directory_reader.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <dirent.h>
+
+
+
+
+int main(){
+
+        DIR *subfolder;
+
+        subfolder = opendir("./test");
+
+        if (subfolder == NULL){
+
+                printf("cant find or read directory");
+        }
+        else{
+                printf("sub directory is open\n");
+
+        }
+}
+        //find out how to read DIR
+        //
+        //
+        //https://c-for-dummies.com/blog/?p=3246
+        //
+        //save DIR and print to screen
+        //
+        //
+        //
+        //
+        //find if lists exist in c
+        //
+        //
+        //
+        //
+        //
+        //add DIR name to list 
+~                                                                                                                                                                                       
+~                                                                                                                                                                                       
+~                  
